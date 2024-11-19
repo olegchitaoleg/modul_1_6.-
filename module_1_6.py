32498be9efc09b4f854665443a62b52e5c7f99ba
olegchitaoleg <jaguar75oleg@gmail.com>
2024-11-19 22:44:26 +0900

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,26 @@
+my_dict = {'Oleg': 1982, 'Misha' : 2005}
+print(my_dict)
+print(my_dict['Oleg'])
+print(my_dict.get('Saha'))
+my_dict.update({'Alex': 856758345, 'Anton': 35757833587})
+print(my_dict)
+a = my_dict.pop('Oleg')
+print(my_dict)
+print(a)
+print(my_dict)
+my_set_ = {4,1,1,8,8,2,1,2,'Стул'}
+print(my_set_)
+print(my_set_.add(6))
+print(my_set_.add(9))
+print(my_set_)
+print(my_set_.discard(1))
+print(my_set_)
+
+
+
+
+
+
+
+
+
